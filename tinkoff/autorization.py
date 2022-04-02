3d8c4c77224813137705d9ab7822bd9f7fab7e6f
david_daimond <daimond0000@mail.ru>
2022-04-02 20:49:29 +0300

    Little changes

--- tinkoff/autorization.py
+++ tinkoff/autorization.py
@@ -2,6 +2,6 @@ import tinvest
 
 
 def sandbox_mod():  # в data/token_sandbox пишите токены свои от сэндбокса
-    with open('data/token_sandbox') as f:  # потом в yaml файл будем складывать, если понадобиться
+    with open('data/token_sandbox') as f:  # потом в yaml файл будем складывать, если понадобится
         client = tinvest.AsyncClient(f.read(), use_sandbox=True)
     return client

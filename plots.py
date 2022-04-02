17e0409c3d9f8ce42ba13eb9920d7828fb9f060d
david_daimond <daimond0000@mail.ru>
2022-04-02 20:49:29 +0300

    Little changes

---
+++ plots.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from config import Saves
+
+
+def stock_prices(value: 'iterable object',
+                 time: 'iterable with time values',
+                 ):
+    pass

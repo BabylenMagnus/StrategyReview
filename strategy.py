48d099017f2404a04ea8f3ebcdae21a5b448793b
Evgeniy <sevostyanikhin02@gmail.com>
2022-04-03 01:52:10 +0300

    shit happend

--- strategy.py
+++ strategy.py
@@ -8,6 +8,7 @@ from typing import Callable
 class Strategy:
     history: list
     decision: Callable
+    current_date: date
 
     def __init__(self, ticket: str, start_date: date, end_date: date):
         self.data = get_shares(ticket)
@@ -20,21 +21,20 @@ class Strategy:
 
         self.ent_point = 0
         self.close_point = 0
-        self.amount = .5
-
-        self.min_per = .1
+        self.amount = .0
 
         self.history = []
 
     def simulation(self):
         open, high, low, close, current_date = self.data[0]
-
+        self.current_date = current_date
         self.ent_point = high
 
         for open, high, low, close, current_date in self.data[1:]:
+            self.current_date = current_date
             self.decision(open, high, low, close)
 
-    def buy(self, stock, price):
+    def buy(self, stock: float, price: float):
         if self.amount == 1:
             return
 
@@ -44,7 +44,7 @@ class Strategy:
             stock -= self.amount - 1
             self.amount = 1
 
-        self.history.append(['Buy', stock, price])
+        self.history.append(['Buy', self.current_date, stock, price])
 
     def sold(self, stock, price):
         if self.amount == 0:
@@ -56,22 +56,67 @@ class Strategy:
             stock += self.amount
             self.amount = 0
 
-        self.history.append(['Sold', stock, price])
+        self.history.append(['Sold', self.current_date, stock, price])
 
 
 class BaseStrategy(Strategy):
-    past: int
 
-    def __init__(self, ticket: str, start_date: date):
-        super(Strategy).__init__(ticket, start_date)
+    def __init__(
+            self, ticket: str, start_date: date, end_date: date, exit_value=0.1, day_ruin=0.02, days2out=12,
+            buy_past_days_down=2, close_past_days=3
+    ):
+        super(Strategy).__init__(ticket, start_date, end_date)
+        self.st_stock = .1
+        self.amount = .5
 
-        self.close_down_age = 0
+        self.value_down_days = 0
+        self.close_up_days = 0
+
+        self.past = 0
+
+        self.next_day_sold = False
+
+        self.day_in_row = 0
+
+        self.exit_value = exit_value
+        self.day_ruin = day_ruin
+        self.days2out = days2out
+        self.buy_past_days_down = buy_past_days_down
+        self.close_past_days = close_past_days
 
     def decision(self, open, high, low, close):
-        if self.past > close:
-            self.close_down_age += 1
-        elif self.close_down_age > 2:
-            self.amount += self.min_per
-            self.amount = max()
+
+        if self.day_in_row == self.days2out:
+            self.sold(1, close)
+
+        if ((self.past - low) / self.past) < -self.exit_value:
+            self.sold(1, close)
+
+        if self.next_day_sold:
+            self.sold(self.st_stock, close)
+            self.next_day_sold = False
+
+        value = abs(open - close) // 2
+
+        if self.past >= value:
+            self.value_down_days += 1
         else:
-            self.close_down_age = 0
+            self.value_down_days = 0
+
+        if close > self.past:
+            self.close_up_days += 1
+        else:
+            self.close_up_days = 0
+
+        if self.value_down_days >= self.buy_past_days_down and self.past <= value:
+            self.buy(self.st_stock, close)
+        elif self.amount > 0 and self.close_up_days > self.close_past_days and \
+                ((self.past - low) / self.past) < -self.day_ruin:
+            self.next_day_sold = True
+
+        self.past = close
+
+        if self.amount > 0:
+            self.day_in_row += 1
+        else:
+            self.day_in_row = 0
